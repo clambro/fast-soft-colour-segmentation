82e11b769f03185e18b8beb3c55ddd354376ac93
Costa <clambro.git@gmail.com>
2021-06-06 17:56:05 -0700

    Model skeleton

---
+++ src/model.py
@@ -0,0 +1,29 @@
+import config
+
+
+class FSCSModel:
+    """"""
+
+    def __init__(self, model_path=None):
+        """"""
+        pass
+
+    def train(self, x_train, y_train, x_val, y_val):
+        """"""
+        pass
+
+    def evaluate(self, x):
+        """"""
+        pass
+
+    def optimize_for_one_image(self, img):
+        """"""
+        pass
+
+    def save(self, path):
+        """"""
+        pass
+
+    def _build_model(self):
+        """"""
+        pass
