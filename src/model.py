4cf9a7f25f87aaab0bf1a3765bbe60d1adb89a11
Costa <clambro.git@gmail.com>
2021-06-14 18:10:23 -0700

    Documentation

--- src/model.py
+++ src/model.py
@@ -31,7 +31,13 @@ class FSCSModel:
         pass
 
     def _build_model(self):
-        """"""
+        """Build the model
+
+        Returns
+        -------
+        model : Model
+            The tensorflow model for image segmentation.
+        """
         input_layer = layers.Input((None, None, 3))
 
         pool = input_layer
@@ -51,20 +57,68 @@ class FSCSModel:
         output_layer = self._do_final_layer(layer, down_layers.pop(-1))
 
         model = Model(input_layer, output_layer)
-        model.compile()
         return model
 
     def _do_down_conv(self, layer, num_filters):
+        """Helper function for convolution and downsampling.
+
+        Parameters
+        ----------
+        layer : Layer
+            The layer to operate on.
+        num_filters : int
+            The number of filters for the convolutional layers.
+
+        Returns
+        -------
+        layer : Layer
+            The layer after convolution.
+        pool : Layer
+            The layer after convolution and pooling.
+        """
         layer = self._do_double_conv(layer, num_filters)
         pool = layers.MaxPool2D()(layer)
         return layer, pool
 
     def _do_up_conv(self, up_layer, skip_layer, num_filters, is_final=False):
+        """Helper function for convolution and upsampling.
+
+        Parameters
+        ----------
+        up_layer : Layer
+            The layer to upsample.
+        skip_layer : Layer
+            The layer to concatenate with the upsampled layer.
+        num_filters : int
+            The number of filters for the convolutional layers.
+        is_final : bool
+            Whether or not this is the final layer in the model (suppresses activation and normalization).
+
+        Returns
+        -------
+        Layer
+            The layer after upsampling, concatenation, and convolution.
+        """
         up_layer = layers.UpSampling2D()(up_layer)
         layer = layers.Concatenate()([up_layer, skip_layer])
         return self._do_double_conv(layer, num_filters, is_final)
 
     def _do_final_layer(self, up_layer, skip_layer):
+        """Helper function for the final layer which generates the outputs.
+
+        Parameters
+        ----------
+        up_layer : Layer
+            The layer to upsample.
+        skip_layer : Layer
+            The layer to concatenate with the upsampled layer.
+
+        Returns
+        -------
+        Layer
+            The final output layer with config.NUM_SEGMENTS alpha channels followed by the same number of RGB images
+            concatenated along the channel axis.
+        """
         up_layer = self._do_up_conv(up_layer, skip_layer, 4 * config.NUM_SEGMENTS, is_final=True)
 
         def normalize_final_layer(layer):
@@ -76,6 +130,22 @@ class FSCSModel:
 
     @staticmethod
     def _do_double_conv(layer, num_filters, is_final=False):
+        """Helper function for double convolution.
+
+        Parameters
+        ----------
+        layer : Layer
+            The layer to convolve.
+        num_filters : int
+            The number of filters for the convolutional layers.
+        is_final : bool
+            Whether or not this is the final layer in the model (suppresses activation and normalization).
+
+        Returns
+        -------
+        Layer
+            The layer after convolution.
+        """
         layer = layers.Conv2D(num_filters, 3, padding='same')(layer)
         layer = layers.LeakyReLU(config.LEAKINESS)(layer)
         layer = layers.Conv2D(num_filters, 3, padding='same')(layer)
