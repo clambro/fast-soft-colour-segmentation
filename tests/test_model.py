8b0f030c0ce10b21fa848def762513cdb7f736ac
Costa <clambro.git@gmail.com>
2021-06-14 22:13:52 -0700

    Fixed incorrect test

--- tests/test_model.py
+++ tests/test_model.py
@@ -9,8 +9,8 @@ class TestModel(unittest.TestCase):
     def test_build_predict_model(self):
         """Test that it builds and runs without error."""
         fscs = FSCSModel()
-        img_shape = (1, 2**(config.NUM_SEGMENTS + 2), 2**(config.NUM_SEGMENTS + 2), 3)
-        fscs.model.predict(np.random.random(img_shape))
+        img_height = 2**(config.NUM_DOWNSAMPLE_LAYERS + 2)
+        fscs.model.predict(np.random.random((1, img_height, img_height, 3)))
 
     def test_save_load(self):
         fscs1 = FSCSModel()
